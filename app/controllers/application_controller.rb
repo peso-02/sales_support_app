f431b03ffe76b2526c2f10afa4986048a55864f5
peso-02 <xwpswgzv7j@privaterelay.appleid.com>
2026-02-23 16:49:17 +0900

    Basic認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,18 @@
 class ApplicationController < ActionController::Base
   include Pundit::Authorization
 
+  before_action :basic_auth
+
   # Punditで権限エラーが起きた時の処理
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
+    end
+  end
 
   def user_not_authorized
     flash[:alert] = "この操作を行う権限がありません。"
