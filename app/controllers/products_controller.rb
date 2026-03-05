c90d913b1cfc8b6a33fe8e3fb0cbbd85ad94d433
peso-02 <xwpswgzv7j@privaterelay.appleid.com>
2026-03-05 22:22:11 +0900

    CSV出力機能（商品情報）

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,6 +4,10 @@ class ProductsController < ApplicationController
   # GET /products or /products.json
   def index
     @products = Product.all
+    respond_to do |format|
+      format.html
+        format.csv { send_data @products.to_csv, filename: "products-#{Date.today}.csv" }
+    end
   end
 
   # GET /products/1 or /products/1.json
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,32 @@
 class Product < ApplicationRecord
   has_many :product_prices, dependent: :destroy
   has_many :customers, through: :product_prices
+
+  def self.to_csv
+    require 'csv'
+    
+    CSV.generate(headers: true) do |csv|
+      csv << [
+        "商品コード",
+        "商品名",
+        "入数",
+        "A単価",
+        "B単価",
+        "C単価",
+        "備考"
+      ]
+      
+      all.each do |product|
+        csv << [
+          product.product_code,
+          product.product_name,
+          product.case_quantity,
+          product.standard_price_a,
+          product.standard_price_b,
+          product.standard_price_c,
+          product.notes
+        ]
+      end
+    end
+  end
 end
