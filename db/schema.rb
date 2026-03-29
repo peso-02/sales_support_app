ca5fe65fa128933a8816a4b85d2207e09156402c
peso-02 <xwpswgzv7j@privaterelay.appleid.com>
2026-03-29 23:47:33 +0900

    Usersテーブル修正

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 20260223062811) do
-  create_table "users", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
+  create_table "customers", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "customer_code"
     t.string "customer_name"
     t.integer "closing_day"
