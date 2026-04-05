f3893f4a3ba308a9a376b23697898c7f45b80826
peso-02 <xwpswgzv7j@privaterelay.appleid.com>
2026-04-06 01:35:59 +0900

    role修正
    UserモデルのroleからgeneralをI削除しデフォルトをassistantに変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,26 +1,23 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   # 役割の定義
-  # general: 一般ユーザー（閲覧のみ）
-  # assistant: アシスタント（編集可能）
-  # approver: 承認者（マニュアル承認可能）
-  # admin: 管理者（全権限）
-  enum role: { general: 0, assistant: 1, approver: 2, admin: 3 }
+  # assistant: アシスタント（編集申請可能）
+  # approver: 承認者（編集申請＋承認可能）
+  # admin: 管理者（全権限＋ユーザー管理）
+  enum role: { assistant: 1, approver: 2, admin: 3 }
+
   def role_i18n
     I18n.t("enums.user.role.#{role}")
   end
 
-  # デフォルトはgeneral
+  # デフォルトはassistant
   after_initialize :set_default_role, if: :new_record?
 
   private
 
   def set_default_role
-    self.role ||= :general
+    self.role ||= :assistant
   end
 end
-
